47a932940ab6ef69ad9f4e7c4aa3f6ce0ed36b86
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-10-21 18:39:58 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -26,9 +26,19 @@ export default async function Home() {
         <Categories />
       </div>
 
-      <div className="mt-8">
+      <div className="mb-8 mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses"
+      />
     </div>
   );
 }
