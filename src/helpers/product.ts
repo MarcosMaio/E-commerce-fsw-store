0eb1c4440d194678c5038cbfed39d7b7e3b6cae9
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-10-21 18:16:05 -0300

    fix: ensure correct product total price is calculated

--- src/helpers/product.ts
+++ src/helpers/product.ts
@@ -4,21 +4,21 @@ export interface ProductWithTotalPrice extends Product {
   totalPrice: number;
 }
 
-export const computeProductTotalPrice = (product: Product): ProductWithTotalPrice => {
-    if(product.discountPercentage === 0) {
-      return {
-        ...product,
-        totalPrice: Number(product.basePrice)
-      };
-    }
-
-    const totalPrice = 
-    Number(product.basePrice) * (product.discountPercentage / 100);
-
+export const computeProductTotalPrice = (
+  product: Product,
+): ProductWithTotalPrice => {
+  if (product.discountPercentage === 0) {
     return {
       ...product,
-      totalPrice,
+      totalPrice: Number(product.basePrice),
     };
+  }
+
+  const totalDiscont =
+    Number(product.basePrice) * (product.discountPercentage / 100);
+
+  return {
+    ...product,
+    totalPrice: Number(product.basePrice) - totalDiscont,
+  };
 };
-
-
